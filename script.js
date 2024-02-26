8a402840297f3eb9b74350506433adda60c79913
jansloupensky <jan.sloupensky1@seznam.cz>
2024-02-26 20:44:11 +0100

    clean code

--- script.js
+++ script.js
@@ -4,17 +4,17 @@
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var dropdownBtn = document.getElementById("dropdownBtn");
   var dropdownContent = document.getElementById("dropdownContent");
 
-  dropdownBtn.addEventListener("click", function() {
-      if (dropdownContent.style.display === "block") {
-          dropdownContent.style.display = "none";
-      } else {
-          dropdownContent.style.display = "block";
-      }
-      scrollTop.scrollIntoView({ behavior: 'smooth' });
+  dropdownBtn.addEventListener("click", function () {
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+    scrollTop.scrollIntoView({ behavior: 'smooth' });
   });
 });
 
@@ -47,10 +47,10 @@ document.addEventListener("DOMContentLoaded", function () {
   var scrollButton = document.getElementById("scroll-to-top");
 
   window.onscroll = function () {
-      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-          scrollButton.style.display = "block";
-      } else {
-          scrollButton.style.display = "none";
-      }
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      scrollButton.style.display = "block";
+    } else {
+      scrollButton.style.display = "none";
+    }
   };
 });
